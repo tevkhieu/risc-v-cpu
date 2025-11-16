778f2bfaadd4344bb6d068abe36f302d4af807e6
tevkhieu <tevchhorpoan@gmail.com>
2025-11-16 21:57:05 +0100

    Thelma et Louise - Solann, Yoa
    - Add README
    - Add empty cpu_top

---
+++ README.md
@@ -0,0 +1,3 @@
+# RISC-V CPU
+
+This repository is a personal projects aiming to implement a RISC-V Unpriviledged ISA from [the version 20250508 official specification](https://docs.riscv.org/reference/isa/unpriv/unpriv-index.html)
---
+++ rtl/cpu_top.sv
@@ -0,0 +1,21 @@
+/*
+ ==============================================================================
+ Filename  : cpu_top.sv
+ Project   : risc-v-cpu
+ Author    : Tévchhorpoan Khieu
+ Created   : 2025-11-16
+
+ Description:
+    - Top-level CPU module definition.
+
+ Notes:
+    - WIP
+ ==============================================================================
+*/
+
+module cpu_top (
+    input logic clk,
+    input logic rst_n
+);
+    
+endmodule
